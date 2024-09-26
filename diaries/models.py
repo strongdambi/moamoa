d9a7f1a42672724f5cba7796021ebdab010bd717
KimGyoungmin <min199485@gmail.com>
2024-09-26 15:37:17 +0900

    [Add] Feat/chatbot 1. diaries/models.py 테스트 테이블 주석처리 해놓았습니다 2. diaries/chatbot.py 챗봇의 핵심 로직이 있습니다. 3. views.py 챗봇의 json형식의 response를 토대로 데이터베이스에    저장합니다.
       api : 127.0.0.1:8000/api/v1/diary/chatbot/
       METHOD : POST
      request 방식 : json
      {
       	"message" : "유저입력"
       }
       챗봇에서 분류하는 user_id에 대한 로직이 필요합니다.

--- diaries/models.py
+++ diaries/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# class Plan(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+#     food_expense = models.DecimalField(max_digits=10, decimal_places=2)
+#     transportation_expense = models.DecimalField(max_digits=10, decimal_places=2)
+#     savings = models.DecimalField(max_digits=10, decimal_places=2)
+#     snack_expense = models.DecimalField(max_digits=10, decimal_places=2)
+#     plan_details = models.TextField()
