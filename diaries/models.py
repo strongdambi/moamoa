bdb76d715d5e29148e6a64dc2c1b6f7b976ba1e9
KimGyoungmin <min199485@gmail.com>
2024-09-26 15:49:35 +0900

    간단한 채팅기능

--- diaries/models.py
+++ diaries/models.py
@@ -1,10 +1,10 @@
 # from django.db import models
 
-# class Plan(models.Model):
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-#     food_expense = models.DecimalField(max_digits=10, decimal_places=2)
-#     transportation_expense = models.DecimalField(max_digits=10, decimal_places=2)
-#     savings = models.DecimalField(max_digits=10, decimal_places=2)
-#     snack_expense = models.DecimalField(max_digits=10, decimal_places=2)
-#     plan_details = models.TextField()
+class Plan(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_amount = models.PositiveIntegerField()
+    food_expense = models.PositiveIntegerField()
+    transportation_expense = models.PositiveIntegerField()
+    savings = models.PositiveIntegerField()
+    snack_expense = models.PositiveIntegerField()
+    plan_details = models.TextField()
