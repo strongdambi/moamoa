6d7d12dbd4e40fd3630c47d938155f9d724665de
leeyounghuncom <web3dao@kakao.com>
2024-09-27 23:07:46 +0900

    pip openai 설치 유져 views.py 이름 기능 추가 용돈기입장 채팅, 삭제, 월말결산 추가 시리얼 나이즈 수입, 지출 기능 추가 월말결산, 용돈기입장 모델 추가

--- diaries/models.py
+++ diaries/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+#월말결산
+class Plan(models.Model):
+    child = models.ForeignKey(User, on_delete=models.CASCADE, related_name="plans")
+    parent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="parent_plans")
+    content = models.TextField()
+    year = models.PositiveIntegerField()
+    month = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('child', 'parent', 'year', 'month')
+
+    def __str__(self):
+        return f"{self.child.username}의 {self.year}년 {self.month}월 계획서 - {self.content}"
+
+#용돈기입장
+class FinanceDiary(models.Model):
+    child = models.ForeignKey(User, on_delete=models.CASCADE, related_name="diaries")
+    parent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="parent_diaries")
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    transaction_type = models.CharField(max_length=7)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    today = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.child.username} - {self.title} ({self.transaction_type})"
