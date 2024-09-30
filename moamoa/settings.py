c95ec40a272f9f00ca5a8c22667c2868aaa961ff
yunlowell <dbsdbf95@gmail.com>
2024-09-30 10:20:56 +0900

    feat: settings.py에 static 파일 설정 추가

--- moamoa/settings.py
+++ moamoa/settings.py
@@ -139,6 +139,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',          # 프로젝트 전체에서 사용하는 static 폴더
+    BASE_DIR / 'webs/static',     # webs 앱에서 사용하는 static 폴더
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
