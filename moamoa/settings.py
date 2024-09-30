edfdf6bf2f07417755d780cdc49f27d910ded127
yunlowell <dbsdbf95@gmail.com>
2024-09-30 19:29:30 +0900

    feat: 로고 이미지 뜨게 설정 index.html 로고이미지 보이도록 static 설정

--- moamoa/settings.py
+++ moamoa/settings.py
@@ -158,7 +158,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'webs' / 'static']  # static 경로 추가
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
