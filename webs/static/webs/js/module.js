920d32178fe83d74e756ae60a905369f4f8e543f
yunlowell <dbsdbf95@gmail.com>
2024-09-30 19:53:15 +0900

    webs/static/webs에 css, js 폴더 추가 refac: index.html 코드 수정

---
+++ webs/static/webs/js/module.js
@@ -0,0 +1,19 @@
+export function FooterFunc() {
+	return `
+        <a href="./"><img src="your-home-icon-url" alt="Home"></a>
+        <a href="#">
+            <img src="your-calendar-icon-url" alt="Calendar">
+        </a>
+        <button class="plus-btn">+</button>
+        <a href="#">
+            <img src="your-bell-icon-url" alt="Notifications">
+        </a>
+        <a href="#">
+            <img src="your-profile-icon-url" alt="Profile">
+        </a>
+    `
+}
+
+export function HeaderFunc() {
+	return `<img src="your-logo-url-here" alt="Logo">`
+}
---
+++ webs/static/webs/js/script.js
@@ -0,0 +1,12 @@
+document.querySelector('.kakao-login').addEventListener('click', function () {
+    location.href = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=c39ec02ba741d3bb7d91cc57847a9566&redirect_uri=http://localhost:8000/api/v1/accounts/auth/kakao/callback/';
+});
+
+document.querySelector('.kids-login').addEventListener('click', function () {
+    location.href = 'children.html';
+});
+
+// document.querySelector('.kids-login').addEventListener('click', function () {
+//     alert(31)
+//     location.href = './';
+// });
