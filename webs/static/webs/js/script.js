b1e3953013534b959ba65da462046667f4aa34f0
yunlowell <dbsdbf95@gmail.com>
2024-10-01 19:14:30 +0900

    fix: 토큰 오류로 키즈 계정 추가 버튼 안눌림 수정

--- webs/static/webs/js/script.js
+++ webs/static/webs/js/script.js
@@ -1,14 +1,23 @@
-document.querySelector('.kakao-login').addEventListener('click', function () {
-    location.href = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=c39ec02ba741d3bb7d91cc57847a9566&redirect_uri=http://localhost:8000/api/v1/accounts/auth/kakao/callback/';
-});
+var kakaoLoginButton = document.querySelector('.kakao-login');
+if (kakaoLoginButton) {
+    kakaoLoginButton.addEventListener('click', function () {
+        location.href = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=c39ec02ba741d3bb7d91cc57847a9566&redirect_uri=http://localhost:8000/api/v1/accounts/auth/kakao/callback/';
+    });
+}
 
-document.querySelector('.kids-login').addEventListener('click', function () {
-    location.href = '/webs/children/';
-});
+var kidsLoginButton = document.querySelector('.kids-login');
+if (kidsLoginButton) {
+    kidsLoginButton.addEventListener('click', function () {
+        location.href = '/webs/children/';
+    });
+}
 
-document.querySelector('.add-account').addEventListener('click', function () {
-    location.href = '/webs/children_create/';  // children_create 페이지로 이동
-});
+var addAccountButton = document.querySelector('.add-account');
+if (addAccountButton) {
+    addAccountButton.addEventListener('click', function () {
+        location.href = '/webs/create/';
+    });
+}
 
 // document.querySelector('.kids-login').addEventListener('click', function () {
 //     alert(31)
