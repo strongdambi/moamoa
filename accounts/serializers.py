28dadbd6f6dbc25e86510d8c925dea3ded824079
yunlowell <dbsdbf95@gmail.com>
2024-10-08 10:14:15 +0900

    feat: 부모프로필에 자녀의 현재잔액 표시

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "first_name", "birthday", "email", "parents_id", "encouragement", "images"]
+        fields = ["id", "username", "first_name", "birthday", "email", "parents_id", "encouragement", "images", "total"]
