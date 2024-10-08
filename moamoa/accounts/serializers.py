1ade06aa028f032dd3f19fa2ea91b9d3efab6147
KimGyoungmin <min199485@gmail.com>
2024-10-09 00:46:19 +0900

    채팅완료

--- moamoa/accounts/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "first_name", "birthday", "email", "parents_id", "encouragement", "images", "total"]
