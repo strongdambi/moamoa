0f1808f15ba24688d400ba628f9d741811ac63a1
KimGyoungmin <min199485@gmail.com>
2024-10-11 11:37:12 +0900

    기홍님나이스!

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.views.static
 
 class User(AbstractUser):
     parents = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children',
                                 help_text='부모님 기본 0 | 아이들은 부모님 번호')
     birthday = models.DateField(null=True, blank=True, help_text='생년월일')
-    images = models.ImageField(upload_to='profile_images/', null=True, blank=True, default='images/default_profile.png', help_text='프로필 이미지')
+    images = models.ImageField(upload_to='profile_images/', null=True, blank=True, default='default_profile.png', help_text='프로필 이미지')
     encouragement = models.TextField(blank=True, null=True, help_text='부모님이 자녀에게 남기는 격려 메시지')
     total = models.IntegerField(default=0)
     
